f597e0b3c50df8a4a756ae5757c14f9b298fdeb5
Parth971 <desaiparth971@gmail.com>
2023-04-18 14:34:12 +0530

    Fix: Update Path of folders in args variable

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import os
 from datetime import datetime
 from pathlib import Path
 
-from docx.shared import Inches, Cm
+from docx.shared import Inches
 import docx
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 
-
 BASE_DIR = Path(__file__).resolve().parent
+
+
 class CreateDocument:
     def __init__(self, filename, image_width):
         self.my_doc = docx.Document()
@@ -26,12 +27,11 @@ class CreateDocument:
             run = paragraph.add_run()
             run.add_picture(initial_data[i][1], width=self.image_width)
 
-            if i+1 < len(initial_data):
-                row_cells_label[1].text = f"{initial_data[i+1][0]}"
+            if i + 1 < len(initial_data):
+                row_cells_label[1].text = f"{initial_data[i + 1][0]}"
                 paragraph = row_cells_image[1].paragraphs[0]
                 run = paragraph.add_run()
-                run.add_picture(initial_data[i+1][1], width=self.image_width)
-        # self.table.add_row()
+                run.add_picture(initial_data[i + 1][1], width=self.image_width)
 
     def set_file_data(self, initial_data):
         for file_name_without_extension in sorted(initial_data.keys()):
@@ -40,7 +40,6 @@ class CreateDocument:
             row_cells[0].merge(row_cells[1])
             row_cells[0].text = file_name_without_extension
             p = row_cells[0].paragraphs[0]
-            # p = row_cells[0].add_paragraph(file_name_without_extension.strip())
             p.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
 
             self.set_data(file_data)
@@ -48,24 +47,25 @@ class CreateDocument:
         self.my_doc.save(str(BASE_DIR / self.filename))
 
 
+if __name__ == '__main__':
+    args = (
+        ('My 1', r"/home/root366/Dump/GITHUB/Images-to-Docs/abc"),
+        ('My 2', r"/home/root366/Dump/GITHUB/Images-to-Docs/vcc"),
+        ('My 3', r"/home/root366/Dump/GITHUB/Images-to-Docs/mvp"),
+    )
+    data = {}
+    for folder_name, folder_path in args:
+        files = os.listdir(folder_path)
+        for file in files:
+            file_name_without_ext = file.split('.')[0]
+            data[file_name_without_ext] = data.get(file_name_without_ext, []) + [
+                [folder_name, os.path.join(folder_path, file)]]
 
+    width = Inches(2.8)
 
-args = (
-    ('My 1', r"C:\Users\Parth\Desktop\Currently Working\docs_project\abc"),
-    ('My 2', r"C:\Users\Parth\Desktop\Currently Working\docs_project\qwer"),
-    ('My 3', r"C:\Users\Parth\Desktop\Currently Working\docs_project\rtrt"),
-)
-data = {}
-for folder_name, folder_path in args:
-    files = os.listdir(folder_path)
-    for file in files:
-        file_name_without_ext = file.split('.')[0]
-        data[file_name_without_ext] = data.get(file_name_without_ext, []) + [[folder_name, os.path.join(folder_path, file)]]
-
-
-width = Inches(2.8)
-
-current_time = datetime.now().time()
-create_docs = CreateDocument(filename=f'testing_{current_time.hour}_{current_time.minute}_{current_time.second}.docx', image_width=width)
-create_docs.set_file_data(data)
-
+    current_time = datetime.now().time()
+    create_docs = CreateDocument(
+        filename=f'testing_{current_time.hour}_{current_time.minute}_{current_time.second}.docx',
+        image_width=width
+    )
+    create_docs.set_file_data(data)
